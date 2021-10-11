270c48551d9969fe930fde775b76e43326c1d6c0
ShangZhang4Food <jerryshang.zhang@gmail.com>
2021-10-11 00:18:57 -0400

    Update the EDA analysis and modeling

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is a project aims to predict the pricing of homes. # home_pricing
+# home_pricing
--- data_scaping_cleaning.py
+++ data_scaping_cleaning.py
@@ -4,22 +4,27 @@ Created on Sun Oct 10 20:20:38 2021
 
 @author: jerry
 """
-import opendatasets as od
+
+# import opendatasets as od
 import pandas as pd
-import numpy as np
+import os
 
 ############# Step 1. Extract data from Kaggle and import the data ####################
 
+path = os.getcwd()
 # Import the dataset from Kaggle
-od.download("https://www.kaggle.com/c/house-prices-advanced-regression-techniques")
+# od.download("https://www.kaggle.com/c/house-prices-advanced-regression-techniques")
 
 # Read the training and testing datasets from csv files
-train_df = pd.read_csv("house-prices-advanced-regression-techniques/train.csv", sep = ',')
+# train_df = pd.read_csv("house-prices-advanced-regression-techniques/train.csv", sep = ',')
+### train the csv with removed outliers.
+train_df = pd.read_csv("train_removeO.csv", sep = ',')
 test_df = pd.read_csv("house-prices-advanced-regression-techniques/test.csv", sep = ',')
 
 
 
 ############# Step 2. Dealing with missing values ####################
+
 
 # Convert the categorical variables into dummy variables
 train_df = pd.get_dummies(train_df, dummy_na=False)
@@ -34,12 +39,20 @@ test_df['LotFrontage'] = test_df['LotFrontage'].fillna(0)
 del train_df['GarageYrBlt']
 del test_df['GarageYrBlt']
 
+# Delete ID column
+del train_df['Id']
+del test_df['Id']
+
 # Deal with MasVnrArea
 train_df['MasVnrArea'] = train_df['MasVnrArea'].fillna(0)
 test_df['MasVnrArea'] = test_df['MasVnrArea'].fillna(0)
 
-train_df.to_csv("train_cleaned.csv")
-test_df.to_csv("test_cleaned.csv")
+# Move the dependent variable to the end of the dataframe
+train_df = train_df[[c for c in train_df if c not in ['SalePrice']] 
+       + ['SalePrice']]
+
+train_df.to_csv(path + '\\train_cleaned.csv', index = False)
+test_df.to_csv(path + '\\test_cleaned.csv', index = False)
 
 
 
