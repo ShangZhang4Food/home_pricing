e7ecb4f5dded60966d8e86938516f719dfd83db5
ShangZhang4Food <jerryshang.zhang@gmail.com>
2021-10-11 13:24:01 -0400

    Add the data scraping file

--- data_scraping.py
+++ data_scraping.py
@@ -7,5 +7,9 @@ Created on Mon Oct 11 13:00:51 2021
 
 import opendatasets as od
 
+
 # Import the dataset from Kaggle
 od.download("https://www.kaggle.com/c/house-prices-advanced-regression-techniques")
+
+# You need to obtain Kaggle API from your Kaggle account. 
+# Check this url for reference: https://www.kaggle.com/docs/api 
